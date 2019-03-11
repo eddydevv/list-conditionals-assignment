885d01decbe8da3e56a3eedf9c5ee597984ace16
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-11 09:35:34 -0600

    Complete part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      inputLength: 0
+    }
+    this.onChangeHandler = this.onChangeHandler.bind(this)
+  }
+
+  onChangeHandler = (e) => {
+    let inputLength = e.target.value.length
+    this.setState({
+      inputLength: inputLength
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input type="text" onChange={this.onChangeHandler} />
+          <p>The input is {this.state.inputLength} characters!</p>
         </header>
       </div>
     );
